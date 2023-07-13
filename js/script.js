babfbc4efe88eb29da6fb57dc672b09e2567cc18
josiahsmejkal <138651830+josiahsmejkal@users.noreply.github.com>
2023-07-13 13:18:21 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll",function(){
+	header.classList.toggle ("sticky",window.scrolly > 100);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+window.onscroll= () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('open');
+};
